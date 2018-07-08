edf489d804498985800df012d38bf6a428651130
gordwyin <gordwinsodhiambo@gmail.com>
2018-07-08 14:40:15 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,343 @@
+
+<!DOCTYPE html>
+
+<html>
+<head>
+<title>Home ::papshop</title>
+<!--/tags -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Elite Shoppy Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--//tags -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<link href="css/easy-responsive-tabs.css" rel='stylesheet' type='text/css'/>
+<!-- //for bootstrap working -->
+<link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800" rel="stylesheet">
+<link href='//fonts.googleapis.com/css?family=Lato:400,100,100italic,300,300italic,400italic,700,900,900italic,700italic' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<?php include 'header/header.php';?>
+<!-- banner -->
+	<div id="myCarousel" class="carousel slide" data-ride="carousel">
+		<!-- Indicators -->
+		<ol class="carousel-indicators">
+			<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+			<li data-target="#myCarousel" data-slide-to="1" class=""></li>
+			<li data-target="#myCarousel" data-slide-to="2" class=""></li>
+			<li data-target="#myCarousel" data-slide-to="3" class=""></li>
+			<li data-target="#myCarousel" data-slide-to="4" class=""></li> 
+		</ol>
+		<div class="carousel-inner" role="listbox">
+			<div class="item active"> 
+				<div class="container">
+					<div class="carousel-caption">
+						<h3>Finde space  <span>for your products/ideas</span></h3>
+						<p>Special for today</p>
+						<a class="hvr-outline-out button2" href="#.html">Find Space </a>
+						<a class="hvr-outline-out button2" href="sign-up/sign-up.php">List Space </a>
+					</div>
+				</div>
+			</div>
+			<div class="item item2"> 
+				<div class="container">
+					<div class="carousel-caption">
+						<h3>We <span>connect you to spaces</span></h3>
+						<p>Both within and oustide Nairobi</p>
+						<a class="hvr-outline-out button2" href="#.html">Find Space </a>
+						<a class="hvr-outline-out button2" href="sign-up/sign-up.php">List Space </a>
+					</div>
+				</div>
+			</div>
+			<div class="item item3"> 
+				<div class="container">
+					<div class="carousel-caption">
+						<h3>We are a network for brands,  <span>retailers </span></h3>
+						<p>Special for today</p>
+						<a class="hvr-outline-out button2" href="#.html">Find Space </a>
+						<a class="hvr-outline-out button2" href="sign-up/sign-up.php">List Space </a>
+					</div>
+				</div>
+			</div>
+			<div class="item item4"> 
+				<div class="container">
+					<div class="carousel-caption">
+						<h3>and  <span>space owners</span></h3>
+						<p>New spaces on papshop</p>
+						<a class="hvr-outline-out button2" href="#.html">Find Space </a>
+						<a class="hvr-outline-out button2" href="sign-up/sign-up.php">List Space </a>
+					</div>
+				</div>
+			</div>
+			<div class="item item5"> 
+				<div class="container">
+					<div class="carousel-caption">
+						<h3>The Biggest <span>Sale</span></h3>
+						<p>Special for today</p>
+						<a class="hvr-outline-out button2" href="#.html">Find Space </a>
+						<a class="hvr-outline-out button2" href="sign-up/sign-up.php">List Space </a>
+					</div>
+				</div>
+			</div> 
+		</div>
+		<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+			<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
+		<!-- The Modal -->
+    </div>
+	<div>space here</div>
+	<!-- schedule-bottom -->
+	<div class="schedule-bottom">
+		<div class="col-md-6 agileinfo_schedule_bottom_left">
+			<img src="images/mid.jpg" alt=" " class="img-responsive" />
+		</div>
+		<div class="col-md-6 agileits_schedule_bottom_right">
+			<div class="w3ls_schedule_bottom_right_grid">
+				<h3>How <span>it</span> Works</h3>
+				<p>How it works..................</p>
+				<div class="col-md-4 w3l_schedule_bottom_right_grid1">
+					<i class="fa fa-user-o" aria-hidden="true"></i>
+					<h4>Clients</h4>
+					<h5 class="counter">653</h5>
+				</div>
+				<div class="col-md-4 w3l_schedule_bottom_right_grid1">
+					<i class="fa fa-calendar-o" aria-hidden="true"></i>
+					<h4>Events</h4>
+					<h5 class="counter">23</h5>
+				</div>
+				<div class="col-md-4 w3l_schedule_bottom_right_grid1">
+					<i class="fa fa-shield" aria-hidden="true"></i>
+					<h4>Spaces</h4>
+					<h5 class="counter">45</h5>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+		<div class="clearfix"> </div>
+	</div>
+<!-- //schedule-bottom -->
+	
+	<div class="banner-bootom-w3-agileits">
+	<div class="container">
+		<h3 class="wthree_text_info">Trending <span>Spaces</span></h3>
+	
+		<div class="col-md-5 bb-grids bb-left-agileits-w3layouts">
+			<a href="wo#.html">
+			   <div class="bb-left-agileits-w3layouts-inner grid">
+					<figure class="effect-roxy">
+							<img src="images/bb1.jpg" alt=" " class="img-responsive" />
+							<figcaption>
+								<h3><span>Meeting</span>Rooms </h3>
+								<p>Find Space</p>
+							</figcaption>			
+						</figure>
+			    </div>
+			</a>
+		</div>
+		<div class="col-md-7 bb-grids bb-middle-agileits-w3layouts">
+		      <a href="#.html">
+		       <div class="bb-middle-agileits-w3layouts grid">
+			           <figure class="effect-roxy">
+							<img src="images/bottom3.jpg" alt=" " class="img-responsive" />
+							<figcaption>
+								<h3><span>Meeting</span>Rooms </h3>
+								<p>Find Space</p>
+							</figcaption>			
+						</figure>
+		        </div>
+				</a>
+				<a href="#.html">
+		      <div class="bb-middle-agileits-w3layouts forth grid">
+						<figure class="effect-roxy">
+							<img src="images/bottom4.jpg" alt=" " class="img-responsive">
+							<figcaption>
+								<h3><span>S</span>ale </h3>
+								<p>Find Space</p>
+							</figcaption>		
+						</figure>
+					</div>
+					</a>
+		<div class="clearfix"></div>
+	</div>
+	</div>
+    </div>
+	
+	<!-- //banner -->
+    <div class="banner_bottom_agile_info">
+	    <div class="container">
+            <div class="banner_bottom_agile_info_inner_w3ls">
+    	           <div class="col-md-6 wthree_banner_bottom_grid_three_left1 grid">
+						<figure class="effect-roxy">
+							<img src="images/bottom1.jpg" alt=" " class="img-responsive" />
+							<figcaption>
+								<h3><span>F</span>all Ahead</h3>
+								<p>New Spaces</p>
+							</figcaption>			
+						</figure>
+					</div>
+					 <div class="col-md-6 wthree_banner_bottom_grid_three_left1 grid">
+						<figure class="effect-roxy">
+							<img src="images/bottom2.jpg" alt=" " class="img-responsive" />
+							<figcaption>
+								<h3><span>F</span>all Ahead</h3>
+								<p>New Spaces</p>
+							</figcaption>			
+						</figure>
+					</div>
+					<div class="clearfix"></div>
+		    </div> 
+		 </div> 
+    </div>
+	
+  <!-- banner-bootom-w3-agileits -->
+	<!-- /we-offer -->
+		<div class="sale-w3ls">
+			<div class="container">
+				<h6>Place <span>Your</span> Space here</h6>
+ 
+				<a class="hvr-outline-out button2" href="single.html">Place Space </a>
+			</div>
+		</div>
+	<!-- //we-offer -->
+<!--/grids-->
+<div class="coupons">
+		<div class="coupons-grids text-center">
+			<div class="w3layouts_mail_grid">
+				<div class="col-md-3 w3layouts_mail_grid_left">
+					<div class="w3layouts_mail_grid_left1 hvr-radial-out">
+						<i class="fa fa-question" aria-hidden="true"></i>
+					</div>
+					<div class="w3layouts_mail_grid_left2">
+						<h3>Have a question</h3>
+						<p>......................</p>
+					</div>
+				</div>
+				<div class="col-md-3 w3layouts_mail_grid_left">
+					<div class="w3layouts_mail_grid_left1 hvr-radial-out">
+						<i class="fa fa-headphones" aria-hidden="true"></i>
+					</div>
+					<div class="w3layouts_mail_grid_left2">
+						<h3>24/7 SUPPORT</h3>
+						<p>...................................</p>
+					</div>
+				</div>
+				<div class="col-md-3 w3layouts_mail_grid_left">
+					<div class="w3layouts_mail_grid_left1 hvr-radial-out">
+						<i class="fa fa-shopping-bag" aria-hidden="true"></i>
+					</div>
+					<div class="w3layouts_mail_grid_left2">
+						<h3>MONEY BACK GUARANTEE</h3>
+						<p>....................................</p>
+					</div>
+				</div>
+					<div class="col-md-3 w3layouts_mail_grid_left">
+					<div class="w3layouts_mail_grid_left1 hvr-radial-out">
+						<i class="fa fa-table" aria-hidden="true"></i>
+					</div>
+					<div class="w3layouts_mail_grid_left2">
+						<h3>Best place</h3>
+						<p>.....................................</p>
+					</div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+
+		</div>
+</div>
+<!--grids-->
+<!-- footer -->
+<?php include 'footer/footer.php';?>
+<!-- js -->
+<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+<!-- //js -->
+<script src="js/modernizr.custom.js"></script>
+	<!-- Custom-JavaScript-File-Links --> 
+	<!-- cart-js -->
+	<script src="js/minicart.min.js"></script>
+<script>
+	// Mini Cart
+	paypal.minicart.render({
+		action: '#'
+	});
+
+	if (~window.location.search.indexOf('reset=true')) {
+		paypal.minicart.reset();
+	}
+</script>
+
+	<!-- //cart-js --> 
+<!-- script for responsive tabs -->						
+<script src="js/easy-responsive-tabs.js"></script>
+<script>
+	$(document).ready(function () {
+	$('#horizontalTab').easyResponsiveTabs({
+	type: 'default', //Types: default, vertical, accordion           
+	width: 'auto', //auto or any width like 600px
+	fit: true,   // 100% fit in a container
+	closed: 'accordion', // Start closed if in accordion view
+	activate: function(event) { // Callback function if tab is switched
+	var $tab = $(this);
+	var $info = $('#tabInfo');
+	var $name = $('span', $info);
+	$name.text($tab.text());
+	$info.show();
+	}
+	});
+	$('#verticalTab').easyResponsiveTabs({
+	type: 'vertical',
+	width: 'auto',
+	fit: true
+	});
+	});
+</script>
+<!-- //script for responsive tabs -->		
+<!-- stats -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<script src="js/jquery.countup.js"></script>
+	<script>
+		$('.counter').countUp();
+	</script>
+<!-- //stats -->
+<!-- start-smoth-scrolling -->
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/jquery.easing.min.js"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+<!-- here stars scrolling icon -->
+	<script type="text/javascript">
+		$(document).ready(function() {
+			/*
+				var defaults = {
+				containerID: 'toTop', // fading element id
+				containerHoverID: 'toTopHover', // fading element hover id
+				scrollSpeed: 1200,
+				easingType: 'linear' 
+				};
+			*/
+								
+			$().UItoTop({ easingType: 'easeOutQuart' });
+								
+			});
+	</script>
+<!-- //here ends scrolling icon -->
+
+
+<!-- for bootstrap working -->
+<script type="text/javascript" src="js/bootstrap.js"></script>
+</body>
+</html>
